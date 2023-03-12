38c3bc11d7794b496e41f0df2630444dc5754860
Santosh Bandichode <santosh.bandichode@cyberium.info>
2023-03-12 22:05:59 +0530

    Include pytest.ini

--- tests/test_wordminer.py
+++ tests/test_wordminer.py
@@ -7,4 +7,11 @@ def test_wordminer():
     assert len(wordminer.words) == 113809
 
 
-# Run with python -m pytest tests/
+# pytest does not add the current directory in the PYTHONPATH itself, but
+# here's a workaround (to be executed from the root of your repository):
+#
+# python -m pytest tests/
+#
+# It works because Python adds the current directory in the PYTHONPATH for you.
+#
+# But with pytest.ini just using pytest is enough.
