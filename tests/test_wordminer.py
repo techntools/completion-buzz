3c153c9ca348b6474b703b0bf49440d1df12204c
Santosh Bandichode <santosh.bandichode@cyberium.info>
2023-03-12 21:51:47 +0530

    Add helper comment for testing command with pytest

--- tests/test_wordminer.py
+++ tests/test_wordminer.py
@@ -5,3 +5,6 @@ def test_wordminer():
     wordminer = WordMiner()
     wordminer.update_words_per_file('./tests/words.txt')
     assert len(wordminer.words) == 113809
+
+
+# Run with python -m pytest tests/
